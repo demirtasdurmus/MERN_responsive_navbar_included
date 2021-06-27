8c25769df73df09574750ae617b841ac6163bcbb
Durmuş <70111575+demirtasdurmus@users.noreply.github.com>
2021-06-27 23:15:34 +0300

    Update SignUp.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,39 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import axios from "axios";
 import { Link } from "react-router-dom";
 
 
 export default function SıgnUp() {
-    const [inputs, setInputs] = useState({
-        name: "",
-        email: "",
-        password: "",
-    });
-
-    const handleInputChange = (e) => {
-        e.preventDefault();
-        const { name, value } = e.target;
-        setInputs({ ...inputs, [name]: value });
-    };
-
-    const handleRegisterUser = (name, email, password) => {
-        axios.post("/api/users/register",
-            {
-                name: name,
-                email: email,
-                password: password
-            })
-            .then((res) => {
-                console.log(res.data.message);
-                window.location.assign('/login');
-            }).catch((err) => {
-                console.log(err.response.data.message);
-                alert(err.response.data.message)
-            })
-    };
-
-    useEffect(() => {
-    }, [])
     return (
         <div className="home">
             Sign Up
